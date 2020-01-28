715e551de52b7eee3cea34c77ab380824431f9ee
Patrick Delaney and Rachel Schieferstein <patrick@>
2020-01-28 14:15:23 -0800

    fixed typos and reiterated functions to prototypes

--- js/scripts.js
+++ js/scripts.js
@@ -4,41 +4,81 @@ function Ticket(movie, age, time ) {
   this.time = time
 }
 
+// function displayTicket(ticketToDisplay) {
+//   var ticketList = $("ul#prices");
+//   var htmlForTicket = "";
+//   ticketToDisplay.Ticket.foreach(function(ticket){
+//     htmlForTicket += "<li>" + Ticket.movie + " " + Ticket.age + " " + ticket.time + "</li>";
+//   })
+// }
+
 $(document).ready(function() {
-  attachContactListeners();
-  $("form#nticketform").submit(function(event) {
+  $("form#ticketForm").submit(function(event) {
     event.preventDefault();
     var movie = $("#movie").val();
     var age = $("#age").val();
     var time = $("#time").val();
     var basePrice = 8; 
+    var newTicket = new Ticket(movie, age, time);
 
-    function price(){
-      if(age == "senior" || age == "child") {
-        return basePrice - 2;
+    Ticket.prototype.ageDiscount = function() {
+      if(this.age === "senior" || this.age === "child") {
+        return - 2;
+      } else { 
+        return 0;
       }
-      if (time == "11am" || time == "1pm"){
-        return basePrice - 2;
-      }
-      if (movie == "m1" || movie == "m2") {
-        return basePrice - 1;
-      }
-      else {
-        return basePrice; 
-      }
-    };
-  
-  Ticket.prototype.ageDiscount = function() {
-    if(age === "senior" || age == "child") {
-      return basePrice - 2;
-    } else { return 0
+    }
+    alert("fuuuuu");
+    Ticket.prototype.timeDiscount = function() {
+      if (this.time == "11am" || this.time == "1pm"){
+        return -2;
+    } else {
+      return 0;
     }
   }
+  
+  Ticket.prototype.movieDiscount = function() {
+    if (this.movie == "m1" || this.movie == "m2") {
+      return - 1;
+    }
+    else {
+      return 0;
+  }
+  }
+  
+  Ticket.prototype.getPrice = function() {
+    return this.ageDiscount() + this.timeDiscount() + this.movieDiscount() + basePrice;
+  }
+  
+  var ticketPrice = newTicket.getPrice();
+
+  console.log(ticketPrice);
+  $("#prices").empty();
+  $("#prices").append(ticketPrice);
 
 
-    price();
+  // displayTicketPrice(ticketPrice);
 
-    $("#output").text()
+  // $("ul#prices").append("<li>" + "ticketPricehaha" + "</Li>");
+
+    // function price(){
+    //   if(age == "senior" || age == "child") {
+    //     return basePrice - 2;
+    //   }
+    //   if (time == "11am" || time == "1pm"){
+    //     return basePrice - 2;
+    //   }
+    //   if (movie == "m1" || movie == "m2") {
+    //     return basePrice - 1;
+    //   }
+    //   else {
+    //     return basePrice; 
+    //   }
+    // };
+
+    
+
+
   });
 
   });
